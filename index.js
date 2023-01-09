9856f1c55fe51f594dc7cf38923d8412de8ef9c4
Philip Demand <philipdemand@yahoo.com>
2023-01-09 14:57:07 -0500

    commit tree a3bf0f5e007351c7fd597e5bd4dfeb0cc66ce073

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(array, name) {
+    const result = array.filter(word => word.toUpperCase() === name.toUpperCase())
+    return result
+  }
+
+  function fuzzyMatch(array, name) {
+    const result = array.filter(word => word.charAt(0) === name.charAt(0))
+    return result
+  }
+
+  function matchName(array, name) {
+    const result = array.filter(word => word.name === name)
+    return result
+  }
